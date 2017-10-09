1ada2ef297544372b7875846796a94cfeea3fd54
Vladimir Ipatov <vs@ipatov.net>
2017-10-09 19:04:31 +0300

    fix build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 rm -rf ./sci-web
 rm -f ./build
 cp -fr ../sci_webinterface ./sci-web
-tar czf ../sci-web_3.0.orig.tar.gz ./README.md debian/changelog debian/compat debian/control debian/copyright debian/dirs debian/install debian/rules debian/source sci-web nginx.conf build.sh uwsgi.conf
+rm -rf ./sci-web/.git ./sci-web/.gitignore
+tar czf ../sci-web_3.0.orig.tar.gz ./README.md debian/changelog debian/compat debian/control debian/copyright debian/dirs debian/install debian/rules debian/source sci-web build.sh sci-web.conf sci-web.ini
 dpkg-buildpackage -rfakeroot
