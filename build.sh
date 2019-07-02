97113297cef25e65d81ada50fc6692378c7b339b
Vladimir Ipatov <vs@ipatov.net>
2019-07-02 19:14:24 +0300

    small fixes

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 rm -rf ./sci-web
 rm -f ./build
 cp -fr ../sci_webinterface ./sci-web
-rm -rf ./sci-web/.git ./sci-web/.gitmodules ./sci-web/.gitignore ./sci-web/django_gentelella/.gitignore ./sci-web/django_gentelella/.gitignore
-tar czf ../sci-web_3.0.orig.tar.gz ./README.md debian/changelog debian/compat debian/control debian/copyright debian/dirs debian/install debian/rules debian/source sci-web build.sh config
+rm -rf ./sci-web/.git ./sci-web/.gitmodules ./sci-web/.gitignore ./sci-web/django_gentelella/.gitignore ./sci-web/django_gentelella/.git
+tar czf ../sci-web_3.1.orig.tar.gz ./README.md debian/changelog debian/compat debian/control debian/copyright debian/dirs debian/install debian/rules debian/source sci-web build.sh config
 dpkg-buildpackage -rfakeroot
