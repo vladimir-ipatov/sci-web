700bcc591ea4343147a093d97fc5e3755d176861
Vladimir Ipatov <vs@ipatov.net>
2020-05-08 09:52:27 +0300

    new version

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@ rm -rf ./sci-web
 rm -f ./build
 cp -fr ../sci_webinterface ./sci-web
 rm -rf ./sci-web/.git ./sci-web/.gitmodules ./sci-web/.gitignore ./sci-web/django_gentelella/.gitignore ./sci-web/django_gentelella/.git
-tar czf ../sci-web_3.2.orig.tar.gz ./README.md debian/changelog debian/compat debian/control debian/copyright debian/dirs debian/install debian/rules debian/source sci-web build.sh config
+tar czf ../sci-web_3.3.orig.tar.gz ./README.md debian/changelog debian/compat debian/control debian/copyright debian/dirs debian/install debian/rules debian/source sci-web build.sh config
 dpkg-buildpackage -rfakeroot
